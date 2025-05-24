fa41dace2783cce532272c121e0b771a68deb46b
m m <kmkl>
2025-05-24 07:39:28 -0400

    first message

---
+++ components/HeaderMenu.tsx
@@ -0,0 +1,17 @@
+import {headerData} from "@/constants"
+import Link from "next/link"
+import React from 'react'
+
+const HeaderMenu = () => {
+  return ( 
+    <div className="hidden md:inline-flex w-1/3 items-center gap-5 text-sm capitalize font-semibold">
+        {headerData?.map((item) => (
+            <Link key={item?.title} href={item?.href}>
+                {item?.title}
+            </Link>
+        ))}
+    </div>
+  )
+}
+
+export default HeaderMenu
---
+++ components/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderMenu from './HeaderMenu'
+
+const Logo = () => {
+  return (
+    <div>
+     Logo
+    </div>
+  )
+}
+
+export default Logo
